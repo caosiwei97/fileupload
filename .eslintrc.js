9f8d1e6747de764eded9f60e3b7dcb471fdc045b
caosiwei <caosiwei97@163.com>
2022-06-18 15:50:53 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,16 @@
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser',
+  parser: '@typescript-eslint/parser',
   // 配置环境变量
   env: {
     browser: true,
     es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true, // vue3 编译宏，可以直接使用 defineProps
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   // 配置规则
-  extends: [
-    'plugin:vue/vue3-strongly-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
     'prettier/prettier': 'error', // 不符合prettier格式化规则的都标记为错误
     '@typescript-eslint/no-empty-function': 'off',
-    'vue/multi-word-component-names': 'off',
   },
 }
--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['../../.eslintrc.js'],
+  parser: 'vue-eslint-parser',
+  extends: ['plugin:vue/vue3-strongly-recommended', '../../.eslintrc.js'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
 }
