85d8a0e81c905a77933c68c8d966b23cf556741d
小火柴 <1286612323@qq.com>
2022-06-29 17:40:09 +0800

    feat: 异常处理

--- packages/axios/src/xhr.ts
+++ packages/axios/src/xhr.ts
@@ -2,11 +2,16 @@ import { AxiosPromise, AxiosRequestConfig } from '../types'
 import { AxiosResponse } from '../types/index'
 import { transformResponse } from './helpers/data'
 import { parseHeaders } from './helpers/headers'
+import { AxiosError } from "./helpers/error";
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { url, method = 'get', data = null, headers, responseType } = config
+    const { url, method = 'get', data = null, headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
+
+    if (timeout) {
+      request.timeout = timeout;
+    }
 
     request.open(method.toUpperCase(), url)
 
@@ -33,6 +38,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         return
       }
 
+      // 断网
+      if (request.status === 0) {
+        return
+      }
+
       const responseData =
         responseType && responseType !== 'text' ? request.response : request.responseText
 
@@ -45,7 +55,32 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         request,
       }
 
-      resolve(response)
+      handleResponse(response)
+
+      function handleResponse(response: AxiosResponse): void {
+        if (response.status >= 200 && response.status < 300) {
+          resolve(response);
+        } else {
+          reject(createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request.status,
+            response
+          ));
+        }
+      }
+    }
+
+    // 监听网络错误事件
+    request.onerror = function () {
+      reject(createError("Net Error", config, null, request));
+    };
+
+    // 监听超时事件
+    request.ontimeout = function () {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, "TIMEOUT", request));
     }
   })
 }
+
